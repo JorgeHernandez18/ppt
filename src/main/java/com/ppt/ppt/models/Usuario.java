d231ff8806ab5f7f3c35e38cb4186c82b70ec72b
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2023-05-27 17:23:12 -0500

    Creación de la entidad Usuario

---
+++ src/main/java/com/ppt/ppt/models/Usuario.java
@@ -0,0 +1,33 @@
+package com.ppt.ppt.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Usuario")
+public class Usuario {
+
+    @Id
+    @Column(name = "cedula")
+    private String cedula;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellido")
+    private String apellido;
+
+    @Column(name = "numeroTelefono")
+    private String numeroTelefono;
+
+    @Column(name = "correoElectronico")
+    private String correoElectronico;
+
+    @Column(name = "rol_id")
+    private int rol_id;
+
+}
