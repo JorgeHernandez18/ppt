caf41510e1725228cffab52d1c2b7df8328df812
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2023-05-27 17:30:35 -0500

    Creación de la entidad DocenteLider

---
+++ src/main/java/com/ppt/ppt/models/DocenteLider.java
@@ -0,0 +1,24 @@
+package com.ppt.ppt.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "DocenteLider")
+public class DocenteLider {
+
+    @Id
+    @Column(name = "codigo")
+    private int codigo;
+
+    @Column(name = "eje_transversal")
+    private int eje_transversal;
+
+    @Column(name = "cedulaDL")
+    private String cedulaDL;
+    
+}
