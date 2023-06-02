2614c0a6c8a04013bd40ff21ff56672ac0a8634c
Jorge Hernandez <jorgekevinhl@ufps.edu.co>
2023-06-01 22:42:01 -0500

    uso de excepciones en la mayoria de controladores

--- src/main/java/com/ppt/ppt/controllers/AuthController.java
+++ src/main/java/com/ppt/ppt/controllers/AuthController.java
@@ -3,14 +3,16 @@ package com.ppt.ppt.controllers;
 import com.ppt.ppt.dao.UsuarioDao;
 import com.ppt.ppt.models.Usuario;
 import com.ppt.ppt.utils.JWTUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("auth")
 public class AuthController {
     @Autowired
     private UsuarioDao usuarioDao;
@@ -20,13 +22,16 @@ public class AuthController {
 
     //Funcionando correctamente
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public String login(@RequestBody Usuario usuario) throws Exception{
+    public String login(@RequestBody Usuario usuario, HttpServletResponse response) throws Exception{
         Usuario usuarioLog = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
         if(usuarioLog != null) {
             System.out.println(usuarioLog.getId() + usuarioLog.getCorreo_electronico());
             System.out.println(jwtUtil.create(String.valueOf(usuarioLog.getId()), usuarioLog.getCorreo_electronico()));
             return jwtUtil.create(String.valueOf(usuarioLog.getId()), usuarioLog.getCorreo_electronico());
         }
+
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+
         return "Correo o contraseña incorrecto";
     }
 }
--- src/main/java/com/ppt/ppt/controllers/PlanTrabajoController.java
+++ src/main/java/com/ppt/ppt/controllers/PlanTrabajoController.java
@@ -2,13 +2,15 @@ package com.ppt.ppt.controllers;
 
 import com.ppt.ppt.dao.PlanTrabajoDao;
 import com.ppt.ppt.models.PlanTrabajo;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/pt")
+@RequestMapping("pt")
 public class PlanTrabajoController {
 
     @Autowired
@@ -18,20 +20,31 @@ public class PlanTrabajoController {
     @RequestMapping(value = "api/plantrabajo", method = RequestMethod.GET)
     public List<PlanTrabajo> getPlanTrabajo(){ return planTrabajoDao.getPlanTrabajo();}
 
-    //Funciona correctamente, sin control de id
+    //Funciona correctamente, con control de id
     @RequestMapping(value = "api/plantrabajo/{id}", method = RequestMethod.GET)
-    public PlanTrabajo getPlanTrabajo(@PathVariable int id){
+    public PlanTrabajo getPlanTrabajo(@PathVariable int id, HttpServletResponse response){
+        if(planTrabajoDao.getPlanTrabajo(id) == null){
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
         return planTrabajoDao.getPlanTrabajo(id);
     }
 
+    //Con control de id
     @RequestMapping(value = "api/plantrabajo/{id}", method = RequestMethod.DELETE)
-    public void deletePlanTrabajo(@PathVariable int id){
+    public void deletePlanTrabajo(@PathVariable int id, HttpServletResponse response){
+
+        if(planTrabajoDao.getPlanTrabajo(id) == null){
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
         planTrabajoDao.deletePlanTrabajo(id);
     }
 
     //Funciona correctamente, sin control de id
     @RequestMapping(value = "api/plantrabajo/{id}", method = RequestMethod.PUT)
-    public void updatePlanTrabajo(@RequestBody PlanTrabajo planTrabajo, @PathVariable int id){
+    public void updatePlanTrabajo(@RequestBody PlanTrabajo planTrabajo, @PathVariable int id, HttpServletResponse response){
+        if(planTrabajoDao.getPlanTrabajo(id) == null){
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+        }
         planTrabajoDao.updatePlanTrabajo(planTrabajo,id);
     }
 
