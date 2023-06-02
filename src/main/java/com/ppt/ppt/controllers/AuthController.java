f2e77400cea94f51fe8d446976e908deb806b271
Johan Leonardo Ibarra Sandoval <johanleonardois@ufps.edu.co>
2023-06-02 08:44:56 -0500

    Cambio en el retorno del token en login

--- src/main/java/com/ppt/ppt/controllers/AuthController.java
+++ src/main/java/com/ppt/ppt/controllers/AuthController.java
@@ -1,11 +1,15 @@
 package com.ppt.ppt.controllers;
 
 import com.ppt.ppt.dao.UsuarioDao;
+import com.ppt.ppt.models.Token;
 import com.ppt.ppt.models.Usuario;
 import com.ppt.ppt.utils.JWTUtil;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,16 +26,16 @@ public class AuthController {
 
     //Funcionando correctamente
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public String login(@RequestBody Usuario usuario, HttpServletResponse response) throws Exception{
+    public ResponseEntity<Token> login(@RequestBody Usuario usuario, HttpServletResponse response) throws Exception{
         Usuario usuarioLog = usuarioDao.obtenerUsuarioPorCredenciales(usuario);
         if(usuarioLog != null) {
             System.out.println(usuarioLog.getId() + usuarioLog.getCorreo_electronico());
             System.out.println(jwtUtil.create(String.valueOf(usuarioLog.getId()), usuarioLog.getCorreo_electronico()));
-            return jwtUtil.create(String.valueOf(usuarioLog.getId()), usuarioLog.getCorreo_electronico());
+            response.setContentType("application/json");
+            return ResponseEntity.ok(new Token(jwtUtil.create(String.valueOf(usuarioLog.getId()), usuarioLog.getCorreo_electronico())));
         }
 
         response.setStatus(HttpStatus.BAD_REQUEST.value());
-
-        return "Correo o contraseña incorrecto";
+        throw new ErrorResponseException(HttpStatusCode.valueOf(400), new Exception("Usuario o contrasenia incorrectos"));
     }
 }
