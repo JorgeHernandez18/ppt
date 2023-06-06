4505e13ecf414b8b82c36c7560c56a7acd8f93fa
Johan Leonardo Ibarra Sandoval <johanleonardois@ufps.edu.co>
2023-06-06 08:28:18 -0500

    Cors

--- src/main/java/com/ppt/ppt/configuration/CorsConfig.java
+++ src/main/java/com/ppt/ppt/configuration/CorsConfig.java
@@ -10,14 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*") // Permitir todas las URL de origen
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir los métodos HTTP
-                .allowedHeaders("*") // Permitir todas las cabeceras
-                .allowCredentials(true) // Permitir enviar credenciales (cookies, encabezados de autenticación, etc.)
-                .maxAge(3600); // Configurar el tiempo máximo de almacenamiento en caché de las opciones pre-vuelo (preflight)
-
-        registry.addMapping("/**")
-                .allowedOriginPatterns("https://ppt-front-uivchwxb3a-uc.a.run.app/") // Permitir todas las URL de origen
+                .allowedOriginPatterns("*") // Permitir todas las URL de origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Permitir los métodos HTTP
                 .allowedHeaders("*") // Permitir todas las cabeceras
                 .allowCredentials(true) // Permitir enviar credenciales (cookies, encabezados de autenticación, etc.)
